983a282da623381e54d1a274814996cfcfb123fb
willywin99 <willywinsianturi@gmail.com>
2022-03-24 21:00:11 +0700

    membuat http server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Hello, we gonna build OpenMusic API');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
