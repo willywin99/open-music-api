47461c766c21f0603ba0f643b91e71981cb89eb0
willywin99 <willywinsianturi@gmail.com>
2022-03-29 17:48:09 +0700

    Membuat router albums dan songs

---
+++ src/api/albums/routes.js
@@ -0,0 +1,31 @@
+// const {
+//   addAlbumHandler,
+//   getAlbumByIdHandler,
+//   editAlbumByIdHandler,
+//   deleteAlbumByIdHandler,
+// } = require('../../handler');
+
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: handler.postAlbumHandler,
+  },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: handler.getAlbumByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: handler.putAlbumByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: handler.deleteAlbumByIdHandler,
+  },
+];
+
+module.exports = routes;
