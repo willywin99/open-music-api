c8914717d7c5429e830048d2962dd1df1dfc68af
willywin99 <willywinsianturi@gmail.com>
2022-03-30 12:33:30 +0700

    Membuat Fungsi Handler pada Albums dan Songs

---
+++ src/api/albums/handler.js
@@ -0,0 +1,91 @@
+/* eslint-disable require-jsdoc */
+class AlbumsHandler {
+  constructor(service) {
+    this._service = service;
+  }
+
+  postAlbumHandler(request, h) {
+    try {
+      const {name = 'untitled', year, body} = request.payload;
+
+      const albumId = this._service.addAlbum({name, year, body});
+
+      const response = h.response({
+        status: 'success',
+        message: 'Album berhasil ditambahkan',
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(400);
+      return response;
+    }
+  }
+
+  getAlbumByIdHandler(request, h) {
+    try {
+      const {id} = request.params;
+      const album = this._service.getAlbumById(id);
+      return {
+        status: 'success',
+        data: {
+          album,
+        },
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  putAlbumByIdHandler(request, h) {
+    try {
+      const {id} = request.params;
+
+      this._service.editAlbumById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Album berhasil ditambahkan',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  deleteAlbumByIdHandler(request) {
+    try {
+      const {id} = request.params;
+      this._service.deleteAlbumById(id);
+      return {
+        status: 'success',
+        message: 'Album berhasil dihapus',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: 'Album gagal dihapus. Id tidak ditemukan',
+      });
+      response.code(404);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ src/api/songs/handler.js
@@ -0,0 +1,111 @@
+/* eslint-disable require-jsdoc */
+class SongsHandler {
+  constructor(service) {
+    this._service = service;
+  }
+
+  postSongHandler(request, h) {
+    try {
+      const {
+        title = 'untitled',
+        year,
+        genre,
+        performer,
+        duration,
+        albumId,
+      } = request.payload;
+
+      // eslint-disable-next-line max-len
+      const songId = this._service.addSong({title, year, genre, performer, duration, albumId});
+
+      const response = h.response({
+        status: 'success',
+        message: 'Lagu berhasil ditambahkan',
+        data: {
+          songId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(400);
+      return response;
+    }
+  }
+
+  getSongsHandler() {
+    const songs = this._service.getSongs();
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  getSongByIdHandler(request) {
+    try {
+      const {id} = request.params;
+      const song = this._service.getSongById(id);
+
+      return {
+        status: 'success',
+        data: {
+          song,
+        },
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  putSongByIdHandler(request, h) {
+    try {
+      const {id} = request.params;
+
+      this._service.editSongById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Lagu berhasil diperbarui',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  deleteSongByIdHandler(request) {
+    try {
+      const {id} = request.params;
+      this._service.deleteSongById(id);
+
+      return {
+        status: 'success',
+        message: 'Lagu berhasil dihapus',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: 'Lagu gagal dihapus. Id tidak ditemukan',
+      });
+      response.code(404);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
