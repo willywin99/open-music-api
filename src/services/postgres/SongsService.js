d8b6236a622a83433524b900ff541d6134f119cb
willywin99 <willywinsianturi@gmail.com>
2022-04-05 01:55:46 +0700

    Membuat Fungsi addAlbum dan addSong

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -1,8 +1,31 @@
 /* eslint-disable require-jsdoc */
+const {nanoid} = require('nanoid');
 const {Pool} = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class SongsService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addSong({title, year, genre, performer, duration, albumId}) {
+    const id = 'song-' + nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      // eslint-disable-next-line max-len
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      // eslint-disable-next-line max-len
+      values: [id, title, year, genre, performer, duration, albumId, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
