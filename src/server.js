1712ed82a0592d647989124e9808732ca52d8a0e
willywin99 <willywinsianturi@gmail.com>
2022-05-03 16:54:20 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -14,9 +14,15 @@ const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
 const ClientError = require('./exceptions/ClientError');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     // port: 5000,
@@ -46,6 +52,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
